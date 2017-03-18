e0c8da1cafdc669fca72e80c49061680c887b459
Tyrant Davis <davistyrant@gmail.com>
2017-03-18 11:47:51 -0700

    Implement #get_roadmap

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,13 +13,18 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(base_url('users/me'), headers: { authorization: @auth_token } )
+    response = self.class.get(base_url('users/me'), headers: { authorization: @auth_token })
     @data = JSON.parse(response.body)
   end
 
   def get_mentor_availability
     response = self.class.get(base_url('mentors/539470/student_availability'), headers: {authorization: @auth_token})
-      @mentor_availability = JSON.parse(response.body)
+    @mentor_availability = JSON.parse(response.body)
+  end
+
+  def get_roadmap
+    response = self.class.get(base_url('roadmaps/31'), headers: { authorization: @auth_token })
+    @roadmap = JSON.parse(response.body)
   end
 
 
